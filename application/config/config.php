9f9f75d56a5ac1b184a953563db0a858d91ba99e
msadiguna152 <50732885+msadiguna152@users.noreply.github.com>
2022-03-17 10:33:45 +0800

    upload

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/pnc/';
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/pnc2/';
 $config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
